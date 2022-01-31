dff515c76eb5f24fc96493edf6b08dc8f9444d57
ashenbrownfox <newhoopsdynasty@gmail.com>
2022-01-30 21:21:59 -0500

    Add intial models

---
+++ restaurantbackend/Models/OrderMaster.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace restaurantbackend.Models
+{
+    public class OrderMaster
+    {
+        [Key]
+        public long OrderMasterId { get; set; }
+        [Column(TypeName = "nvarchar(75)")]
+        public string OrderNumber { get; set; }
+        public int CustomerId { get; set; }
+        [Column(TypeName = "nvarchar(10)")]
+        public string PaymentMethod { get; set; }
+        public decimal GrandTotal { get; set; }
+    }
+}
